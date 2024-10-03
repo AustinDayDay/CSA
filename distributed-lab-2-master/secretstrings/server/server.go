26055a2e394e242b6f1ae92fd5bf06a94db3fc12
AustinDayDay <iv23829@bristol.ac.uk>
2024-10-03 15:48:05 +0100

    editted client and server

--- distributed-lab-2-master/secretstrings/server/server.go
+++ distributed-lab-2-master/secretstrings/server/server.go
@@ -28,11 +28,11 @@ func (s *SecretStringOperations) Reverse(req stubs.Request, res stubs.Response) 
 }
 
 func main() {
-	pAddr := flag.String("port", ":8080", "Port to listen on")
+	pAddr := flag.String("port", ":8030", "Port to listen on")
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
 	rpc.Register(&SecretStringOperations{})
 	listener, _ := net.Listen("tcp", ":"+*pAddr)
-	defer listener.Close()
+	//defer listener.Close()
 	rpc.Accept(listener)
 }
