a26054e038ae08c3e24c14344c35d857c18c5d48
AustinDayDay <iv23829@bristol.ac.uk>
2024-10-08 16:27:39 +0100

    added colon

--- distributed-lab-2-master/secretstrings/server/server.go
+++ distributed-lab-2-master/secretstrings/server/server.go
@@ -33,11 +33,11 @@ func (s *SecretStringOperations) FastReverse(req stubs.Request, res stubs.Respon
 }
 
 func main() {
-	pAddr := flag.String("port", ":8030", "Port to listen on")
+	pAddr := flag.String("port", "8030", "Port to listen on")
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
 	rpc.Register(&SecretStringOperations{})
-	listener, _ := net.Listen("tcp", *pAddr)
+	listener, _ := net.Listen("tcp", ":"+*pAddr)
 	defer listener.Close()
 	rpc.Accept(listener)
 }
