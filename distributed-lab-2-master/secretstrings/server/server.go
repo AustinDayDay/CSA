2cdfb30094fdb7ef38079e2c78f7c605ef1384f7
AustinDayDay <iv23829@bristol.ac.uk>
2024-10-08 16:12:22 +0100

    test

--- distributed-lab-2-master/secretstrings/server/server.go
+++ distributed-lab-2-master/secretstrings/server/server.go
@@ -38,7 +38,7 @@ func main() {
 	pAddr := flag.String("port", ":8030", "Port to listen on")
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
-	rpc.Register(&SecretStringOperations{})
+	rpc.Register(SecretStringOperations{})
 	listener, _ := net.Listen("tcp", *pAddr)
 	defer listener.Close()
 	rpc.Accept(listener)
